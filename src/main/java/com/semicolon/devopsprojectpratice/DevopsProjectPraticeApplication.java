f4649f2356a0279c7860082cc1ad79e4839acf18
HibatullahiShakira <Hibatullahishakira@gmail.com>
2025-02-28 12:02:38 +0100

    first commit

---
+++ src/main/java/com/semicolon/devopsprojectpratice/DevopsProjectPraticeApplication.java
@@ -0,0 +1,13 @@
+package com.semicolon.devopsprojectpratice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsProjectPraticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevopsProjectPraticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/semicolon/devopsprojectpratice/DevopsProjectPraticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.semicolon.devopsprojectpratice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsProjectPraticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
